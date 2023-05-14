f42b08caf4e7afbbb58e4e3a57213cebdef6d42f
ngongo0108 <thuhuong562002@gmail.com>
2023-05-14 14:16:17 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Player from './components/Players';
+function App() {
+  return (
+    <div className="App">
+     <Navigation/>
+     <Player/>
+    </div>
+  );
+}
+
+export default App;
